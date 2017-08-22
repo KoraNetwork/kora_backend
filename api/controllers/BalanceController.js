26e8479bb48326fd26cb38e25831105b8c9fc332
Roman Katsala <rkatsala@gmail.com>
2017-08-22 15:20:30 +0300

    Add testing etherium/web3 integration

---
+++ api/controllers/BalanceController.js
@@ -0,0 +1,55 @@
+/**
+ * BalanceController
+ *
+ * @description :: Server-side logic for managing balances
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var Web3 = require('web3');
+var web3 = new Web3();
+
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+module.exports = {
+  /**
+   * `BalanceController.show()`
+   */
+  show: function (req, res) {
+    // var result = {};
+    // var totalBal = 0;
+    //
+    // for (var acctNum in eth.accounts) {
+    //   var acct = eth.accounts[acctNum];
+    //   var acctBal = web3.fromWei(eth.getBalance(acct), "ether");
+    //
+    //   result[acctNum] = {
+    //     account: acct,
+    //     balance: acctBal,
+    //   };
+    //
+    //   totalBal += parseFloat(acctBal);
+    //   console.log("  eth.accounts[" + acctNum + "]: \t" + acct + " \tbalance: " + acctBal + " ether");
+    // }
+    //
+    // result.totalBalance = totalBal;
+    // console.log("  Total balance: " + totalBal + " ether");
+
+    // var balance = web3.eth.getBalance("0x407d73d8a49eeb85d32cf465507dd71d507100c1");
+    // console.log(balance); // instanceof BigNumber
+    // console.log(balance.toString(10)); // '1000000000000'
+    // console.log(balance.toNumber()); // 1000000000000
+
+    var coinbase = web3.eth.coinbase;
+    console.log(coinbase);
+
+    var balance = web3.eth.getBalance(coinbase);
+    console.log(balance.toString(10));
+
+    var acctBal = web3.fromWei(balance, "ether");
+
+    return res.json({
+      coinbase,
+      balance: acctBal,
+    });
+  }
+};
