09de62bd31aa1d9c6e8804cc3a696563a029a660
Roman Katsala <rkatsala@gmail.com>
2017-08-28 17:44:05 +0300

    Set production db to localDiscDb for Azure test

--- config/env/production.js
+++ config/env/production.js
@@ -13,13 +13,23 @@
 module.exports = {
 
   /***************************************************************************
+   * Azure environment variables                 *
+   ***************************************************************************/
+
+  // Use process.env.port to handle web requests to the default HTTP port
+  port: process.env.port,
+  // Increase hooks timout to 30 seconds
+  // This avoids the Sails.js error documented at https://github.com/balderdashy/sails/issues/2691
+  hookTimeout: 30000,
+
+  /***************************************************************************
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+  models: {
+    connection: 'localDiskDb'
+  },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
@@ -34,15 +44,5 @@ module.exports = {
   // log: {
   //   level: "silent"
   // },
-
-  /***************************************************************************
-   * Azure environment variables                 *
-   ***************************************************************************/
-
-  // Use process.env.port to handle web requests to the default HTTP port
-  port: process.env.port,
-  // Increase hooks timout to 30 seconds
-  // This avoids the Sails.js error documented at https://github.com/balderdashy/sails/issues/2691
-  hookTimeout: 30000,
 
 };
