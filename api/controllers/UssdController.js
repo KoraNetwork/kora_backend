ca8ce5b6e1d528b37bdc1c9bc3c319797b641e9c
Roman Katsala <rkatsala@gmail.com>
2017-08-28 19:34:16 +0300

    Add testing logic for Africa's talking USSD gateway

--- api/controllers/UssdController.js
+++ api/controllers/UssdController.js
@@ -13,9 +13,10 @@ module.exports = {
    * `UssdController.callback()`
    */
   callback: function (req, res) {
-    var body = req.body || { data: 'No data was sent' };
+    var body = req.body || { data: 'No data was send' };
+    var text = 'END Sent data: ' + JSON.stringify(body);
 
-    return res.json(body);
+    return res.send(text);
   }
 };
 
