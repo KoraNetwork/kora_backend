0b708f8357cd81bee358cd5063c24e6071f7a723
Roman Katsala <rkatsala@gmail.com>
2017-08-29 13:57:38 +0300

    Add Twilio and sms test action

---
+++ api/controllers/SmsController.js
@@ -0,0 +1,41 @@
+/**
+ * SmsController
+ *
+ * @description :: Server-side logic for managing sms
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+ var accountSid = 'AC93c494dcc34b88d2e6e1b6b2411bc0a1'; // Your Account SID from www.twilio.com/console
+ var authToken = '8d7c804369655c43a73b842d8808bec4';   // Your Auth Token from www.twilio.com/console
+
+ var twilio = require('twilio');
+ var client = new twilio(accountSid, authToken);
+
+
+module.exports = {
+
+
+
+  /**
+   * `SmsController.test()`
+   */
+  test: function (req, res) {
+		client.messages.create({
+		    body: 'Hello from Kora MVP',
+		    to: '+380956641698',  // Text this number
+		    from: '+14159361767' // From a valid Twilio number
+		})
+		.then((message) => {
+			sails.log('SMS message was send: ', message.sid);
+
+			return res.json({
+				message: 'SMS message was send succesfully',
+				// data: message,
+				sid: message.sid,
+			})
+		})
+		.catch((err) => {
+			return res.negotiate(err);
+		});
+  }
+};
