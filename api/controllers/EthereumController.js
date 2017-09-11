b48bbda93c313dc9886f0355b0ae0269569622f8
Roman Katsala <rkatsala@gmail.com>
2017-09-11 17:37:44 +0300

    Add testing creation of encrypted account

--- api/controllers/EthereumController.js
+++ api/controllers/EthereumController.js
@@ -5,6 +5,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+/*global sails */
+
 var Web3 = require('web3');
 
 var Accounts = require('web3-eth-accounts');
@@ -19,9 +21,11 @@ module.exports = {
    */
   create: function (req, res) {
     let acc = accounts.create(Web3.utils.randomHex(32));
+    let keystore = accounts.encrypt(acc.privateKey, 'qwer1234');
 
-    sails.log.info('New ethereum account created:', acc);
+    sails.log.info('New ethereum account created:\n', keystore);
+    sails.log.info('Private key test: ', acc.privateKey === accounts.decrypt(keystore, 'qwer1234').privateKey);
 
-    return res.json(acc);
+    return res.json(keystore);
   }
 };
