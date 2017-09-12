93c7e1b9c8dde566f39aa7826e4bb1119c55a941
Roman Katsala <rkatsala@gmail.com>
2017-09-12 14:25:26 +0300

    Add ethereum account generation when creating new user

--- api/models/User.js
+++ api/models/User.js
@@ -5,6 +5,8 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+/* global sails EthereumService */
+
 module.exports = {
   attributes: {
     phone: { type: 'string', unique: true, required: true },
@@ -12,5 +14,30 @@ module.exports = {
     address: { type: 'string' },
 
     keystore: { type: 'json' }
+  },
+
+  beforeCreate: function (values, cb) {
+    let password = values.password;
+
+    if (!password) {
+      if (sails.config.environment === 'development') {
+        // Password for development purposes
+        password = 'qwer1234';
+      } else {
+        cb(new Error('Password must be set'));
+      }
+    }
+
+    EthereumService.createAccount({ password }, function (err, { address, keystore }) {
+      if (err) {
+        return cb(err);
+      }
+
+      values.address = address;
+      values.keystore = keystore;
+      delete values.password;
+
+      return cb();
+    });
   }
 };
