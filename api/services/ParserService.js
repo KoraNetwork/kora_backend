b71ba8749ff71dbc9f3facbeff71a79507b3fee2
SHabor <gabor_96@mail.ru>
2017-09-12 17:38:10 +0300

    Fixes & next step.

--- api/services/ParserService.js
+++ api/services/ParserService.js
@@ -8,6 +8,10 @@ module.exports = {
         result = "1: Check Balance\n2: Deposit/Withdraw\n3: Send Money\n4: Request Money\n5: Lend/Borrow";
         break;
       }
+      case "3": {
+        result = "Please enter contact\n OR select:\n 2, 3, 4, 5: List\nfavorite/recent\ncontacts";
+        break;
+      }
       default: {
         result = "Kora MVP are coming! Head for the hills!"
       }
--- api/services/TrialService.js
+++ api/services/TrialService.js
@@ -4,7 +4,7 @@ var trial_string = "Sent from your Twilio trial account";
 
 module.exports = {
   getMessage: function(str) {
-    if(str.indexOf(trial_string) > -1) return str.substr(trial_string.length + 3)
+    if(str.indexOf(trial_string) > -1) return str.substr(trial_string.length + 3);
     else return str;
   }
 };
