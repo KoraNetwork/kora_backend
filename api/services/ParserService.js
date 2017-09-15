dfc1bc53f110c688494644c2bb7badbc497fe0d8
SHabor <gabor_96@mail.ru>
2017-09-15 15:57:54 +0300

    SMS: Request Money.

--- api/services/ParserService.js
+++ api/services/ParserService.js
@@ -10,12 +10,18 @@ module.exports = {
         break;
       }
       case "3": {
+
+      }
+      case "4": {
         result = "Please enter contact\n OR select:\n 2, 3, 4, 5: List\nfavorite/recent\ncontacts";
         break;
       }
       default: {
-        if(ParserService.isNumber(message)){
-          result = "Please enter amount"
+        if(isNumber(message)){
+          result = "Please enter amount (format: $xxx)"
+        }
+        else if(message[0] == '$' && parseInt(message.substr(1)).toString() == message.substr(1)){
+          result = "Please confirm you want to request " + message + " from 555-555-5555 by entering your PIN.";
         }
         else{
           result = "Kora MVP are coming! Head for the hills!"
