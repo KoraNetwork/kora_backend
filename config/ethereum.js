9eaac75a56733f383148d4ba91a0339d596bc75d
Roman Katsala <rkatsala@gmail.com>
2017-09-18 13:42:11 +0300

    Add networkId to ethereum config

--- config/ethereum.js
+++ config/ethereum.js
@@ -3,40 +3,18 @@
  * @type {Object}
  */
 
- // const net = require('net');
- // const Web3 = require('web3');
- // const web3 = new Web3('/home/roma/.rinkeby/geth.ipc', net);
-
 module.exports.ethereum = {
 
   /**
    * Ethereum provider
    * @type {String}
    */
-  // Local testnet
-  provider: 'http://localhost:8545'
-  // provider: web3.currentProvider
-  // provider: 'ws://localhost:8546'
+  // provider: 'http://localhost:8545',
+  // provider: 'ws://localhost:8546',
 
-  // Main Ethereum Network
-  // provider: 'https://mainnet.infura.io/hgr5j4WqYnr4EAbXKZXt'
-
-  // Test Ethereum Network (Ropsten)
-  // provider: 'https://ropsten.infura.io/hgr5j4WqYnr4EAbXKZXt'
-
-  // Test Ethereum Network (Rinkeby)
-  // provider: 'https://rinkeby.infura.io/hgr5j4WqYnr4EAbXKZXt'
-
-  // Test Ethereum Network (Kovan)
-  // provider: 'https://kovan.infura.io/hgr5j4WqYnr4EAbXKZXt'
-
-  // Test Ethereum Network (INFURAnet)
-  // provider: 'https://infuranet.infura.io/hgr5j4WqYnr4EAbXKZXt'
-
-  // IPFS Gateway
-  // provider: 'https://ipfs.infura.io'
-
-  // IPFS RPC
-  // provider: 'https://ipfs.infura.io:5001'
-
+  /**
+   * Network id
+   */
+  networkId: 4 // Rinkeby testnet
+  // networkId: 42 // Kovan testnet
 };
