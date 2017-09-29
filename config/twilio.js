ddb2070886559ec5048caa349ef78a5c2b6b3ba3
Roman Katsala <rkatsala@gmail.com>
2017-09-29 15:15:31 +0300

    Change Twilio account

--- config/twilio.js
+++ config/twilio.js
@@ -7,16 +7,19 @@ module.exports.twilio = {
   /**
    * Account SID from www.twilio.com/console
    */
-  accountSid: 'AC93c494dcc34b88d2e6e1b6b2411bc0a1',
+  // accountSid: 'AC93c494dcc34b88d2e6e1b6b2411bc0a1', // namtab2k
+  accountSid: 'ACe7ca55bdaeb8a8cbeb7f53aef67b8993',
 
   /**
    * Auth Token from www.twilio.com/console
    */
-  authToken: '8d7c804369655c43a73b842d8808bec4',
+  // authToken: '8d7c804369655c43a73b842d8808bec4', // namtab2k
+  authToken: '3bcbf7e99cc2917b3721216672641e0b',
 
   /**
    *  From a valid Twilio number
    */
-  fromNumber: '+14159361767'
+  // fromNumber: '+14159361767' // namtab2k
+  fromNumber: '+19124175349'
 
 };
