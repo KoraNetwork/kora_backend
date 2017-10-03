6b34060bc965a80347e9dacb668d0f517bc26686
Roman Katsala <rkatsala@gmail.com>
2017-10-03 13:36:23 +0300

    Add web3-utils and update versions

--- api/services/ValidationService.js
+++ api/services/ValidationService.js
@@ -3,10 +3,10 @@
  * @description :: Set of validation functions
  */
 
-const Web3 = require('web3');
+const Web3Utils = require('web3-utils');
 
 module.exports = {
   phoneNumber: value => /^[1-9]\d{9,11}$/i.test(value),
 
-  address: value => Web3.utils.isAddress(value)
+  address: value => Web3Utils.isAddress(value)
 };
