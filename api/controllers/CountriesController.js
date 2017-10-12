39f0b23357a52c3356925fce5ab47f0757b733c7
Roman Katsala <rkatsala@gmail.com>
2017-10-12 12:30:44 +0300

    Add ERC20Tokens to countries

--- api/controllers/CountriesController.js
+++ api/controllers/CountriesController.js
@@ -5,20 +5,17 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+/* global sails */
 const worldCountries = require('world-countries');
-const countriesFilter = [
-  'CA',
-  'NG',
-  'UA',
-  'UG',
-  'US'
-];
+const {ERC20Tokens} = sails.config.countries;
 
 const parsedCountries = worldCountries
-  .filter(el => ~countriesFilter.indexOf(el.cca2))
+  .filter(el => ~Object.keys(ERC20Tokens).indexOf(el.cca2))
   .map(({name: {common: name}, currency: [currency], callingCode: [callingCode], cca2}) => ({
     name,
+    countryCode: cca2,
     currency,
+    ERC20Token: ERC20Tokens[cca2],
     phoneCode: `+${callingCode}`,
     flag: `/images/flags/${cca2}.png`
   }));
---
+++ config/countries.js
@@ -0,0 +1,13 @@
+/**
+ * Countries/currencies settings
+ */
+
+module.exports.countries = {
+  ERC20Tokens: {
+    DE: '0x2f381331a4296ecad24f9768a763fd9c3de82942',
+    GB: '0x65ad14fdaa1e76ea4e470448c9f45f367c61f6e0',
+    NG: '0x1f719f39f32e3994504c35a8b112a0476f6f00a4',
+    UA: '0x1d29e459f353901e5200db56ef5d6e7ccf772f4d',
+    US: '0x545053968018bff704408dacf69bb8f9cacf161f'
+  }
+};
