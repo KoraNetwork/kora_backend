3a0f7a3c5d66fa5268d0a51a21e1cbdbe53cc140
Roman Katsala <rkatsala@gmail.com>
2017-11-13 15:03:18 +0200

    Add createLoan method

--- contracts/KoraLend.sol
+++ contracts/KoraLend.sol
@@ -4,24 +4,56 @@ contract KoraLend {
 
     enum States { Created, Agreed, Funded, PaidBack, Expired }
 
-    struct Guarantor {
-        address addr;
-        bool agree;
-    }
-
     struct Loan {
-        States state;
         address borrower;
         address lender;
-        uint numGuarantors;
-        mapping (uint => Guarantor) guarantors;
+        address[] guarantors;
         uint borrowerAmount;
         uint lenderAmount;
         uint rate;
         uint startDate;
         uint maturityDate;
+        States state;
+        mapping (address => bool) guarantorsAgree;
     }
 
     uint public numLoans;
     mapping (uint => Loan) public loans;
+
+    event LoanCreated(uint loanId);
+
+    modifier limitGuarantorsLength(address[] guarantors) {
+        require(guarantors.length > 0 && guarantors.length <= 3);
+        _;
+    }
+
+    function createLoan(
+        address lender,
+        address[] guarantors,
+        uint borrowerAmount,
+        uint lenderAmount,
+        uint rate,
+        uint startDate,
+        uint maturityDate
+    )
+        public
+        limitGuarantorsLength(guarantors)
+        returns (uint loanId)
+    {
+        loanId = numLoans++;
+
+        loans[loanId] = Loan({
+            borrower: msg.sender,
+            lender: lender,
+            guarantors: guarantors,
+            borrowerAmount: borrowerAmount,
+            lenderAmount: lenderAmount,
+            rate: rate,
+            startDate: startDate,
+            maturityDate: maturityDate,
+            state: States.Created
+        });
+
+        LoanCreated(loanId);
+    }
 }
