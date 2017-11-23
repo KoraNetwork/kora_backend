2c79d68feb9f319dd7933283839bb70c00c56133
Roman Katsala <rkatsala@gmail.com>
2017-11-23 19:54:30 +0200

    update KoraLend address

--- config/ethereum.js
+++ config/ethereum.js
@@ -42,7 +42,7 @@ module.exports.ethereum = {
   KoraLend: {
     networks: {
       4: {
-        address: '0x181790fdB1934fa0b616f2e5C4e0Fce5f98443fE'
+        address: '0x124a6EbEC6ED7Be2DA245eaa78A2ECcA8a7DD64B'
       }
     }
   }
