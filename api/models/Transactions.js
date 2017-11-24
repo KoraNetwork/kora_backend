00cd715cd558582a7350e4cd26abce4cf937da72
Roman Katsala <rkatsala@gmail.com>
2017-11-25 00:52:05 +0200

    Change transaction model lifecycles

--- api/models/Transactions.js
+++ api/models/Transactions.js
@@ -5,7 +5,7 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
-/* global _ sails ValidationService UserValidationService TokensService */
+/* global _ sails ValidationService UserValidationService EthereumService */
 
 const WLError = require('waterline/lib/waterline/error/WLError');
 
@@ -54,9 +54,7 @@ module.exports = {
 
     toAmount: { type: 'float', required: true },
 
-    rawTransactions: { type: 'array', hexArray: true },
-
-    transactionHashes: { type: 'array', hexArray: true },
+    transactionHashes: { type: 'array', hexArray: true, defaultsTo: [] },
 
     additionalNote: { type: 'string' },
 
@@ -103,57 +101,71 @@ module.exports = {
   },
 
   beforeCreate: function (values, cb) {
-    if (!values.rawTransactions.length) {
+    const {rawTransactions} = values;
+
+    if (
+      ~[types.send, types.request].indexOf(values.type) &&
+      !(rawTransactions && rawTransactions.length)
+    ) {
       return cb(new WLError({
         status: 400,
-        reason: 'Parameter rawTransactions must have at least one element'
+        reason: 'Parameter rawTransactions must be set and must have at least one element'
       }));
+    }
+
+    if (rawTransactions) {
+      if (!(Array.isArray(rawTransactions) && rawTransactions.every(tx => ValidationService.hex(tx)))) {
+        return cb(new WLError({reason: `Parameter 'rawTransactions' must be hex array`, status: 400}));
+      }
+
+      this.rawTransactions = rawTransactions;
+      delete values.rawTransactions;
     }
 
     return cb();
   },
 
   afterCreate: function (record, cb) {
-    const { rawTransactions } = record;
+    if (this.rawTransactions) {
+      const rawTransactions = this.rawTransactions;
+      delete this.rawTransactions;
 
-    if (!(rawTransactions && rawTransactions.length)) {
-      return cb();
-    }
-
-    record.transactionHashes = [];
-
-    Promise.all(
-      rawTransactions.map(rawTransaction => TokensService.transferSignedRawTx({rawTransaction}))
-    )
-      .then(receipts => {
-        receipts.forEach(r => record.transactionHashes.push(r.transactionHash));
-        record.state = states.success;
-        record.rawTransactions = [];
-
-        this.update({id: record.id}, record)
-          .then(updated => {
-            // TODO: Add push here
-            sails.log.info('Transaction success state saved:\n', updated[0]);
-          })
-          .catch(err => sails.log.error('Transaction success state save error:\n', err));
-      })
-      .catch(err => {
-        sails.log.error('Signed raw transaction send error:\n', err);
-
-        record.state = states.error;
-        if (err.receipt) {
-          record.transactionHashes.push(err.receipt.transactionHash);
+      Promise.all(
+        rawTransactions.map(rawTransaction => EthereumService.sendSignedTransaction({
+          rawTransaction,
+          name: 'Token.transfer'
+        }))
+      )
+        .then(receipts => {
+          receipts.forEach(r => record.transactionHashes.push(r.transactionHash));
+          record.state = states.success;
           record.rawTransactions = [];
-        }
 
-        this.update({id: record.id}, record).exec((err, updated) => {
-          if (err) {
-            return sails.log.error('Transaction error state save error:\n', err);
+          this.update({id: record.id}, record)
+            .then(updated => {
+              // TODO: Add push here
+              sails.log.info('Transaction success state saved:\n', updated[0]);
+            })
+            .catch(err => sails.log.error('Transaction success state save error:\n', err));
+        })
+        .catch(err => {
+          sails.log.error('Signed raw transaction send error:\n', err);
+
+          record.state = states.error;
+          if (err.receipt) {
+            record.transactionHashes.push(err.receipt.transactionHash);
+            record.rawTransactions = [];
           }
-          // TODO: Add push here
-          sails.log.info('Transaction error state saved:\n', updated[0]);
+
+          this.update({id: record.id}, record).exec((err, updated) => {
+            if (err) {
+              return sails.log.error('Transaction error state save error:\n', err);
+            }
+            // TODO: Add push here
+            sails.log.info('Transaction error state saved:\n', updated[0]);
+          });
         });
-      });
+    }
 
     return cb();
   }
