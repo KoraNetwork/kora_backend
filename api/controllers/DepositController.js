d9f05616e3567ba60f59fecd62fe0282debd3edf
Roman Katsala <rkatsala@gmail.com>
2018-02-02 15:40:35 +0200

    Change transactions from Kora Wallet logic for Deposit

--- api/controllers/DepositController.js
+++ api/controllers/DepositController.js
@@ -40,8 +40,8 @@ module.exports = {
       Deposit.findPopulate({ where, limit, skip, sort, userId }),
       Deposit.count(where)
     ])
-    .then(([data, total]) => res.json({data, total}))
-    .catch(err => res.negotiate(err));
+      .then(([data, total]) => res.json({data, total}))
+      .catch(err => res.negotiate(err));
   },
 
   findOne: function (req, res) {
