5648881ad505952680f540350c2522f9468377a9
Roman Katsala <rkatsala@gmail.com>
2018-02-15 14:15:35 +0200

    Add closeLoan method to LendService

--- api/services/LendService.js
+++ api/services/LendService.js
@@ -5,7 +5,10 @@
 
 /* global sails EthereumService MiscService */
 
-const {provider, networkId, KoraLend} = sails.config.ethereum;
+const {provider, networkId, KoraLend, koraWallet, gas, gasPrice} = sails.config.ethereum;
+
+const Accounts = require('web3-eth-accounts');
+const accounts = new Accounts(provider);
 
 const Contract = require('web3-eth-contract');
 Contract.setProvider(provider);
@@ -90,5 +93,45 @@ module.exports = {
     let promise = koraLend.methods.getLoanGuarantors(loanId).call();
 
     return MiscService.cbify(promise, cb);
+  },
+
+  closeLoan,
+
+  closeLoanExpired: function ({loanId, nonce}, cb) {
+    return closeLoan({loanId, nonce, event: 'LoanExpired'}, cb);
+  },
+
+  closeLoanOverdue: function ({loanId, nonce}, cb) {
+    return closeLoan({loanId, nonce, event: 'LoanOverdue'}, cb);
   }
 };
+
+function closeLoan ({loanId, nonce, event}, cb) {
+  const method = koraLend.methods.closeLoan(loanId);
+  const encodedMethod = method.encodeABI();
+
+  let tx = {
+    to: koraLendAddress,
+    data: encodedMethod,
+    gas,
+    gasPrice,
+    nonce
+  };
+
+  sails.log.info('Sign closeLoan transaction:\n', tx);
+
+  let promise = accounts.signTransaction(tx, koraWallet.privateKey)
+    .then(signedTx => {
+      sails.log.info('Send signed closeLoan transaction:\n', signedTx);
+
+      return EthereumService.sendSignedTransactionWithEvent({
+        rawTransaction: signedTx.rawTransaction,
+        name: 'KoraLend.closeLoan',
+        contract: koraLend,
+        event // 'LoanExpired', 'LoanOverdue'
+      });
+    })
+    .then(({receipt, events}) => ({receipt, event: events[0]}));
+
+  return MiscService.cbify(promise, cb);
+}
